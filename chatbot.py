ea75e960c315555552134b573d22d3de3e6949f8
Nikhil Kanbarkar <sololevelingup100@gmail.com>
2026-07-17 19:55:14 +0530

    Added the file exception and error handling part oin the code

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,20 @@
 from config import client, MODEL_NAME
+from google.genai.errors import ClientError, ServerError
 
 
 def get_ai_response(prompt):
-    response = client.models.generate_content(
-        model=MODEL_NAME,
-        contents=prompt
-    )
-    return response.text
+    try:
+        response = client.models.generate_content(
+            model=MODEL_NAME,
+            contents=prompt
+        )
+        return response.text
+
+    except ClientError as e:
+        return f"Client Error: {e}"
+
+    except ServerError:
+        return "Gemini servers are busy. Please try again later."
+
+    except Exception as e:
+        return f"Unexpected Error: {e}"
